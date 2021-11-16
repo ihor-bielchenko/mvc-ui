2408a6b86a27e482130c61ec40eb2c9a45a45560
ihor <ihor.bielchenko@drivedatum.com>
2021-11-16 03:27:15 +0200

    validate uniques keys for proxy source items in JsObject component

--- src/components/Dialog/SourceProxy/onSave.js
+++ src/components/Dialog/SourceProxy/onSave.js
@@ -1,36 +1,26 @@
 import Store from 'components/Store';
 import getTemplate from 'components/JsObject/getTemplate.js';
 import generateKey from 'components/JsObject/generateKey.js';
+import onDialog from 'components/Dialog/onDialog.js';
+import unique from 'utils/unique.js';
 import { SOURCE_TYPE_PROXY_PASS } from 'structures/sourceTypes.js';
 import { 
 	DATA_TYPE_ATOMIC,
 	DATA_TYPE_OBJECT,
 } from 'structures/dataTypes.js';
+import { DIALOG_KEY_EXISTS } from 'consts/dialog.js';
+import onClose from '../onClose.js';
 
-const onSave = (e, id, onClose) => {
-	e.preventDefault();
-
+const merge = (id, tempValue, sourceValue, onCloseProxy) => {
 	const {
 		jsObject,
 		routes,
 	} = Store().getState();
 	const data = jsObject.data;
 	const blocks = jsObject.blocks;
-	const tempValue = jsObject.tempValue;
 	const routesData = routes.data;
 	const routeId = tempValue.route_id;
 	const route = routesData.find((item) => item.id === routeId);
-	const sourceValue = {
-		...tempValue,
-		source_type_id: SOURCE_TYPE_PROXY_PASS.id,
-		columns: {
-			statusCode: 'statusCode',
-			uri: 'uri',
-			method: 'method',
-			headers: 'headers',
-			data: 'data',
-		},
-	};
 	let newId = Date.now();
 	const currentItem = data[id] || {};
 	const parentId = currentItem.parent_id;
@@ -39,6 +29,21 @@ const onSave = (e, id, onClose) => {
 	const responseParsed = {};
 	let responseKeys = Object.keys(route.response),
 		childId = newId - 999999;
+	const _sourceValue = {
+		...sourceValue,
+		columns: (() => {
+			const collector = {};
+
+			Object
+				.keys(sourceValue.columns)
+				.forEach((key) => {
+					collector[key] = sourceValue.columns[key][1]
+						? unique() +'-'+ sourceValue.columns[key][0]
+						: sourceValue.columns[key][0];
+				});
+			return collector;
+		})()
+	};
 
 	responseKeys.forEach((key) => {
 		if (route.response[key].id > 0) {
@@ -61,7 +66,7 @@ const onSave = (e, id, onClose) => {
 		parentItem.data_type_id = DATA_TYPE_OBJECT.id;
 		currentItem.data_type_id = DATA_TYPE_OBJECT.id;
 		currentItem.key = generateKey(blocks[parentId]);
-		currentItem.value = sourceValue;
+		currentItem.value = _sourceValue;
 		currentItem.disabledType = true;
 		currentItem.disabledRemove = true;
 
@@ -113,7 +118,7 @@ const onSave = (e, id, onClose) => {
 			id: newId,
 			data_type_id: DATA_TYPE_OBJECT.id,
 			key: generateKey(blocks[currentItem.id] ?? []),
-			value: sourceValue,
+			value: _sourceValue,
 			disabledType: true,
 			disabledValue: true,
 			disabledRemove: true,
@@ -151,7 +156,70 @@ const onSave = (e, id, onClose) => {
 			blocks[data[responseParsed[key].id].parent_id].push(data[responseParsed[key].id]);
 		});
 	}
-	onClose();
+	onCloseProxy();
+	onClose(DIALOG_KEY_EXISTS)();
+};
+
+const onSave = (e, id, onClose) => {
+	e.preventDefault();
+
+	const jsObject = Store().getState().jsObject;
+	const data = jsObject.data;
+	const blocks = jsObject.blocks;
+	const tempValue = jsObject.tempValue;
+	const sourceValue = {
+		...tempValue,
+		source_type_id: SOURCE_TYPE_PROXY_PASS.id,
+		columns: {
+			statusCode: [ 'statusCode', false ],
+			uri: [ 'uri', false ],
+			method: [ 'method', false ],
+			headers: [ 'headers', false ],
+			data: [ 'data', false ],
+		},
+	};
+	const currentItem = data[id] || {};
+	const parentId = currentItem.parent_id;
+	const parentItem = data[parentId];
+	const parentDataTypeId = (parentItem || {}).data_type_id || currentItem.data_type_id;
+	let keyExistsFlag = false;
+
+	Object
+		.keys(sourceValue.columns)
+		.forEach((nowColumnKey) => {
+			const findIndex = blocks[parentDataTypeId === DATA_TYPE_ATOMIC.id
+				? parentId
+				: id].findIndex((item) => {
+				let columnExistsFlag = false;
+
+				if ((item.value || {}).columns) {
+					Object
+						.keys((item.value || {}).columns || {})
+						.forEach((columnKey) => {
+							if (((item.value || {}).columns || {})[columnKey] === sourceValue.columns[nowColumnKey][0]) {
+								columnExistsFlag = true;
+								sourceValue.columns[nowColumnKey][1] = true;
+							}
+						});
+					}
+					return item.key === sourceValue.columns[nowColumnKey][0]
+						|| columnExistsFlag;
+				});
+
+			if (findIndex > -1) {
+				keyExistsFlag = true;
+				sourceValue.columns[nowColumnKey][1] = true;
+			}
+		});
+
+	if (keyExistsFlag) {
+		onDialog(DIALOG_KEY_EXISTS, {
+			merge: () => merge(id, tempValue, sourceValue, onClose),
+		})(e);
+	}
+	else {
+		merge(id, tempValue, sourceValue, onClose);
+	}
 };
 
 export default onSave;
